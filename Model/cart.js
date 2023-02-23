7e8e11d14ca21c32e29ae1f414a0098af3a600db
maor <maorsaban10@gmail.com>
2023-02-23 21:49:47 +0200

    updated but need a lot of work

--- Model/cart.js
+++
@@ -1,9 +0,0 @@
-const Article = require('../models/article');
-
-const index = (req, res) => {
-    res.render("../views/articles", { articles: Article.getArticles() });
-}
-
-module.exports = {
-    index
-};
