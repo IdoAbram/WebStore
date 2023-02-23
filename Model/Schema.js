c137204e245ac41d8317e2cd49f2bf9647b888f9
ALeph1 <alonmi0242@gmail.com>
2023-02-23 21:37:39 +0200

    Fixed Schema

--- Model/Schema.js
+++ Model/Schema.js
@@ -11,11 +11,15 @@ const Customer = new mongoose.Schema({
     required: true 
   },
   address: String,
-  moneySpent: Double,
-  wishList: Array,
-  shoppingCart: Array,
-  orders: Array,
-  email: String,
+  moneySpent:{
+    type:Double,
+    default: 0,
+    required: true
+  },
+  wishList: [String], //Every string represents an item (Hashset)
+  shoppingCart: [String], // As above (Hashset)
+  orders: [String], // As above (Order id which will be in a hashset)
+  email: String, 
 
   password: {
     type: String,
