5a74dfa2dd4984e53ef2b0dd3bbbd0407bc84ad1
ALeph1 <alonmi0242@gmail.com>
2023-03-01 14:25:35 +0200

    Added functionality

--- Routes/customers.js
+++ Routes/customers.js
@@ -2,25 +2,19 @@ const express = require("express")
 const router = express.Router();
 const customerController = require('../Controller/Customer')
 
-
 router.get('/get',async (req,res)=>{
-
     const customers = await customerController.getCustomers({});
     res.render("../View/getAllCustomersPage/getCustomers",{customers})
-    //const c = await customerController.getCustomers({})
-    //res.send(c);
 })
 
 router.get('/create',async (req,res)=>{
-
     res.render("../View/makeCustomerPage/makeCustomer")
-    
 })
 
 
 router.route('/create').post(customerController.createCustomer)
-router.route('/delete').get(function (req,res,next){
 
+router.route('/delete').get(function (req,res){
     customerController.deleteAllCustomers
     res.redirect("/customers/get")
 });
--- app.js
+++ app.js
@@ -20,21 +20,17 @@ mongoose.connect("mongodb://127.0.0.1:27017",()=>{console.log("Connected")})
 run()
 
 
-
 async function run(){
 
     const customer = new Customer({Name:"Alon",lastName:"Michaeli",address:"MM",moneySpent:10.0,wishList:["Hello"],shoppingCart:["Hello"],orders:["Hello"],email:"111@gmail.com",password:"1234",creditCards:["Hello"]})
     const customer2 = new Customer({Name:"Ido",lastName: "Shimon",address: "George IV",moneySpent: 1004.4,wishList: ["HogLegacy"],shoppingCart: ["Aleph"],orders:["An Order"],email:"idodi5@gmail.com",password:"213123",creditcards:["334234","43223"]})
   
-   
    /*
     await customer.save().then(()=> console.log("Saved Alon"));
-    
     await customer2.save().then(()=>console.log("Saved Ido"))
   */
-    await console.log(await Customer.count())
-    
 
+    await console.log(await Customer.count())
 }
 
 
