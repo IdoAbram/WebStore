6ab60a82d744947cd58ba8242b499fe905977136
ALeph1 <alonmi0242@gmail.com>
2023-03-04 16:15:41 +0200

    Made search customer generic

--- Controller/customer.js
+++ Controller/customer.js
@@ -8,9 +8,10 @@ const createCustomer = async (req, res) => {
 };
 
 const getCustomers = async (req, res) => { // The service should return Value Set
-    const customers = await customerService.getCustomers();
+    const customers = await customerService.getCustomers(req.body.filter);
     return customers;
 };
+
 
 const getCustomerById = async (req, res, next) => { //The service should put all customers in hashmap
     var customer = await customerService.getCustomerById(req.params.id);
