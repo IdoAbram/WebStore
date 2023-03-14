56d881acc2b15384f4edeb6fc8da04ddc441b3f7
IdoShimon <ido.shimon6@gmail.com>
2023-03-14 13:42:17 +0200

    Fixed product page

--- View/TemplatePage/TemplatePage.js
+++ View/TemplatePage/TemplatePage.js
@@ -3,7 +3,18 @@ const filename="TemplateHead.html"
 const id1="footer";
 const filename1="TemplateFoot.html";
 
+let l=0;
+
 window.onload=function(){
+    if(l==0){
+        l=1;
+        
+        go();
+    }
+}
+
+function go(){
+    
     console.log(`div id: template , filename: TemplateHead.html`);
     let xhttp;
     let element=document.getElementById(id);
@@ -23,12 +34,14 @@ window.onload=function(){
         xhttp.open("GET",`../TemplatePage/${file}`,true);
         xhttp.send();
     }
+    
     //footer
-    console.log(`div id: footer , filename: TemplateFoot.html`);
+    console.log(`div id: footer , filename1: TemplateFoot.html`);
     let xhttp1;
     let element1=document.getElementById(id1);
     let file1=filename1;
     if(file1){
+        
         xhttp1=new XMLHttpRequest();
         xhttp1.onreadystatechange=function(){
             if(this.readyState==4){
@@ -44,8 +57,6 @@ window.onload=function(){
         xhttp1.send();
         return;
     }
-
-
     
-
+    
 }
