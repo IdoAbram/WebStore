a738d3a628b30a4dd7439d3deddb1fe0112a0ab1
ALeph1 <alonmi0242@gmail.com>
2023-03-15 20:46:08 +0200

    Finished Services for Review Schema

--- Routes/prPage.js
+++ Routes/prPage.js
@@ -7,44 +7,6 @@ const customerService = require('../Services/Customer')
 
 
 
-
-router.route('/:idPr/:idCu').get(async (req,res)=>{
-    productID = req.params.idPr;
-    customerID=req.params.idCu;
-    const product= await productService.getProductById(productID);
-    var customer1=0;
-
-    if(customerID!='guest'){
-
-        customer1 = await customerService.getCustomerById(productID);
-    }
-    const customer=customer1;
-    
-
-    if(!product){
-        await res.json({message:"Not Found"})
-        return;
-    }
-    res.render("../View/GenericProductPage/productPage",{customer,product})
-
-
-
-})
-
-// router.route('/:idPr/guest').get(async (req,res)=>{
-    
-//     const product= await productController.getProductById(req,res);
-//     const custoemr=0;
-
-//     if(!product){
-//         await res.json({message:"Not Found"})
-//         return;
-//     }
-//     productID = req.params.id;
-
-//     res.render("../View/GenericProductPage/productPage",{customer,product})
-// })
-
 router.route('/:id').get(async (req,res)=>{
     
     const product= await productController.getProductById(req,res);
---
+++ Services/review.js
@@ -0,0 +1,52 @@
+const Review = require('../Model/Schemas/Review');
+
+const createReview = async (Title,userId,ProductId,text,rating,Images_Videos) => {
+
+    const Review = new Review({Title:Title,userId:userId,ProductId:ProductId,text:text,rating:rating,Images_Videos:Images_Videos})
+
+    return await Review.save();
+};
+
+const getReviewById = async (id) => {
+    return await Review.findById(id);
+};
+
+const getReviews = async (filter) => {
+    return await Review.find(filter);  // Return all Reviews that satsify the filter (json format)
+};
+
+const updateReviewTitle = async (id, Title) => {
+    await Review.findOneAndUpdate({_id:id},{Title:Title});
+};
+
+const updateReviewUserId = async (id, userId) => {
+    await Review.findOneAndUpdate({_id:id},{userId:userId});
+};
+
+const updateReviewProductId = async (id, ProductId) => {
+    await Review.findOneAndUpdate({_id:id},{ProductId:ProductId});
+};
+
+const updateReviewText = async (id, text) => {
+    await Review.findOneAndUpdate({_id:id},{text:text});
+};
+
+const updateReviewRating = async (id, rating) => {
+    await Review.findOneAndUpdate({_id:id},{Rating:rating});
+};
+
+const updateReviewImagesVideos = async (id, ImagesVideos) => {
+    await Review.findOneAndUpdate({_id:id},{Images_Videos:ImagesVideos});
+};
+
+module.exports = {
+    createReview,
+    getReviewById,
+    getReviews,
+    updateReviewTitle,
+    updateReviewUserId,
+    updateReviewProductId,
+    updateReviewText,
+    updateReviewRating,
+    updateReviewImagesVideos
+}
