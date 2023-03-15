758a7442f6197ee04fb83c59bfb53ed5fa4adb89
IdoShimon <ido.shimon6@gmail.com>
2023-03-15 23:25:39 +0200

    Added everything together and transfered data via sessions

--- Routes/HomePage.js
+++ Routes/HomePage.js
@@ -6,42 +6,35 @@ const supplierService = require('../Services/supplier');
 
 
 router.get('/',async (req,res)=>{
-    res.render("../View/HomePage/HomePage")
-})
-
-router.get('/customer/',async (req,res)=>{
+    const type = req.session.userType;
     const id = req.session.user;
     const user = await customerService.getCustomerById(id);
-    if(!user){
-        await res.json({message:"Not Found"})
-        return;
-    }
-    res.render("../View/HomePage/HomePageCustomer",{id,user})
+    
+    res.render("../View/HomePage/HomePage",{id,user,type})
 })
 
-router.get('/admin/',async (req,res)=>{
-    const id = req.session.user;
-    const user = await adminService.getAdminById(id);
-    if(!user){
-        await res.json({message:"Not Found"})
-        return;
-    }
-    res.render("../View/HomePage/HomePageAdmin",{id,user})
-})
 
-router.get('/supplier/',async (req,res)=>{
-    const id = req.session.user;
-    const user = await supplierService.getSupplierById(id);
-    if(!user){
-        await res.json({message:"Not Found"})
-        return;
-    }
-    res.render("../View/HomePage/HomePageSupplier",{id,user})
-})
 
-router.post('/',async (req,res)=>{
-    const products = await productController.getProductsByFilter({});
-    res.render("../View/StorePage/StorePage",{products})
-})
+// router.get('/admin/',async (req,res)=>{
+//     const id = req.session.user;
+//     const user = await adminService.getAdminById(id);
+//     if(!user){
+//         await res.json({message:"Not Found"})
+//         return;
+//     }
+//     res.render("../View/HomePage/HomePageAdmin",{id,user})
+// })
+
+// router.get('/supplier/',async (req,res)=>{
+//     const id = req.session.user;
+//     const user = await supplierService.getSupplierById(id);
+//     if(!user){
+//         await res.json({message:"Not Found"})
+//         return;
+//     }
+//     res.render("../View/HomePage/HomePageSupplier",{id,user})
+// })
+
+
 
 module.exports = router;
