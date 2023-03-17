a35636fe5372ffecc1714f96f8552e6b46b65284
IdoShimon <ido.shimon6@gmail.com>
2023-03-17 13:05:38 +0200

    Devided between the controller and the route in cart route

--- Routes/cart.js
+++ Routes/cart.js
@@ -5,16 +5,6 @@ const customerService = require('../Services/customer')
 
 
 router.get('/',async (req,res)=>{
-   /* const Product = await productController.getProducts(req,res);
-    if(!Product){
-        await res.json({message:"Not Found"})
-        return;
-    }
-
-    const customer = await (cartController.getCustomerById(req,res));
-    const products=customer.shoppingCart
-
-    await res.render("../View/Cart/CartM",{products,Product})*/
     const id=req.session.user;
     const customer = await customerService.getCustomerById(id);
     let products;
@@ -26,17 +16,7 @@ router.get('/',async (req,res)=>{
     let first = false;
     const allProducts = await productController.getProductsByFilter({});
 
-    let finalProducts=[]
-
-    if(products!=null){
-        for(let i=0;i<allProducts.length;i++){
-            if(products.includes(allProducts[i]._id)){
-                if(!finalProducts.includes(allProducts[i])){
-                    finalProducts.push(allProducts[i]);
-                }
-            }
-        }
-    }
+    let finalProducts=productController.getProductsByIds(allProducts,products);
     
     res.render("../View/Cart/CartM",{finalProducts,id,type,user,first})
 })
@@ -48,14 +28,9 @@ router.route('/removeFromCart/:id').get(async (req,res)=>{
     const customer = await customerService.getCustomerById(id);
     
     if(customer.shoppingCart!==null){
-        let products=customer.shoppingCart;
-        let finalProductsIDS=[]
-        for(let i=0;i<products.length;i++){
-            if(products[i]!=productID){
-                finalProductsIDS.push(products[i]);
-            }
-        }
-        customerService.updateCustomerShoppingCart(id,finalProductsIDS);
+        let products= productController.removeFromCart(customer.shoppingCart,productID);
+        
+        customerService.updateCustomerShoppingCart(id,products);
         res.redirect('/cart');
     }
     else{
